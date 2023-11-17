f182bd5316d77580bf7a93c7c2446edd3f6bf77e
Mackson Mesquita <macksondar.mesquita17@gmail.com>
2023-11-17 09:27:47 -0300

    :fire: Css enchancements, gitignore and Accordion modification

--- src/components/Jump/index.tsx
+++ src/components/Jump/index.tsx
@@ -5,8 +5,8 @@ export function Jumptron() {
         <h1 className="text-5xl font-extrabold tracking-tight text-blue-700 mb-4">Conheça a Carpaccio Hunter</h1>
         <h3 className="text-textMain font-bold text-2xl mb-6 tracking-wide">Nós investimos no seu potencial!</h3>
         <p className="mb-8 text-lg font-normal text-textMain lg:text-xl sm:px-16 lg:px-48">
-          Aqui no Carpaccio Hunter nos concentramos em mercados onde tecnologia e culinária, juntamente com a inovação e o capital podem desbloquear valor a longo prazo e impulsionar o crescimento de
-          novas promessas da cozinha!
+          Aqui no Carpaccio Hunter nos concentramos em mercados, onde a tecnologia e a culinária, juntamente com a inovação e o capital podem desbloquear valor a longo prazo e impulsionar o
+          crescimento de novas promessas na cozinha!
         </p>
         <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
           <a
@@ -19,7 +19,7 @@ export function Jumptron() {
             </svg>
           </a>
           <a href="#" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-textMain rounded-lg border border-gray-300 hover:bg-gray-600">
-            Aprender mais
+            Ver mais
           </a>
         </div>
       </div>
